608e53065c4157503222264cb76a5887c263902d
yadhies1989 <yadi.sepriadi@gmail.com>
2022-06-09 15:12:44 +0700

    chalenge 06 done

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,51 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallBack) => {
+  // const kata = [];
+  // file pertama
+  fs.readFile(file1, "utf-8", (err, data) => {
+    if (err) {
+      fnCallBack(err, null);
+    } else {
+      const hasilBacaData = JSON.parse(data);
+      const datasatu = [];
+      datasatu.push(hasilBacaData.message.split(" ")[1]);
+
+      // file kedua
+
+      fs.readFile(file2, "utf-8", (err, data) => {
+        if (err) {
+          fnCallBack(err, null);
+        } else {
+          const hasilBacaData = JSON.parse(data);
+
+          hasilBacaData.forEach((element) => {
+            datadua = [];
+            datadua.push(element.message.split(" ")[1]);
+          });
+
+          // file ketiga
+
+          fs.readFile(file3, "utf8", (err, data) => {
+            if (err) {
+              fnCallBack(err, null);
+            } else {
+              const hasilBacaData = JSON.parse(data);
+
+              hasilBacaData.forEach((element) => {
+                datatiga = [];
+                datatiga.push(element.data.message.split(" ")[1]);
+              });
+
+              fnCallBack(null, [datasatu, datadua, datatiga]);
+            }
+          });
+        }
+      });
+    }
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
